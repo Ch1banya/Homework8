cfa45dcf2c93f2ba2c9dfe906ebdc2303be68b09
ch1ba <ch1banya@bk.ru>
2022-05-29 13:49:20 +0300

    Done

--- src/Homework8/App.java
+++ src/Homework8/App.java
@@ -1,4 +1,57 @@
 package Homework8;
 
-public class App {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class App extends JFrame {
+
+    private final String TEXT = "Генерация номера";
+    int num = 0;
+    public App() {
+        setBounds(500,500,400,200);
+        setTitle("Генератор номеров телефона");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        Font font = new Font("Arial", Font.BOLD, 30);
+
+        JTextField jTextField = new JTextField();
+        JButton jButton = new JButton(TEXT);
+
+        jButton.setFont(font);
+
+
+
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jTextField.setText(generateNumber());
+            }
+        });
+
+        jTextField.setFont(font);
+        jTextField.setHorizontalAlignment(JTextField.CENTER);
+
+        add(jTextField, BorderLayout.CENTER);
+        add(jButton, BorderLayout.SOUTH);
+
+        setVisible(true);
+
+    }
+
+    private String generateNumber() {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("+7");
+        stringBuilder.append("999");
+        for (int i = 0; i < 7; i++) {
+            stringBuilder.append(random.nextInt(10));
+        }
+        return stringBuilder.toString();
+    }
+
+
 }
--- src/Homework8/Main.java
+++ src/Homework8/Main.java
@@ -1,4 +1,7 @@
 package Homework8;
 
 public class Main {
+    public static void main(String[] args) {
+        Homework8.App app = new Homework8.App();
+    }
 }
